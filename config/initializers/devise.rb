7f2b2b246212ee09fbc97f2756f8d57853e2a876
Jason Hobbs <grimmstede@feemon.com>
2015-07-15 15:10:45 -0500

    added google signup and login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,9 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  require 'omniauth-google-oauth2'
+  config.omniauth :google_oauth2, ENV['API_KEY'], ENV['API_SECRET'], {access_type: "offline", approval_prompt: ""}
+
+
 end
