b110cf0fee645cbca0b88b294bb371ac2b172e07
Jason Hobbs <grimmstede@feemon.com>
2015-07-14 20:39:56 -0500

    added mandrill template for new user welcome message

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -2,4 +2,22 @@ class MyMailer < ActionMailer::Base
   def mandrill_client
     @mandrill_client ||= Mandrill::API.new 'lWGGOIL1NHbqKfiNXrdVuQ'
   end
+
+  def new_user(user)
+    template_name = "New User"
+    template_content = []
+    message = {
+      to: [{email: "#{user.email}"}],
+      subject: "welcom to Code4Pro",
+      merge_vars: [
+        {
+          rcpt: user.email,
+          vars: [{name: "USER_NAME", content: user.name}]
+        }
+      ]
+    }
+
+    mandrill_client.messages.send_template template_name, template_content, message
+
+  end
 end
